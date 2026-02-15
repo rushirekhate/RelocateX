3e9421b9ee5b6549ba9affa8ec824247c8d7298d
rushikesh rekhate <rrekhate0@gmail.com>
2026-02-15 14:24:14 +0530

    Domain layer base entity and user entity created

---
+++ backend/RelocateX.API/RelocateX.Domain/Entities/User.cs
@@ -0,0 +1,22 @@
+﻿using RelocateX.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RelocateX.Domain.Entities
+{
+    public class User : BaseEntity
+    {
+        public string FullName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        public string PasswordHash { get; set; } = string.Empty;
+
+        public string Role { get; set; } = "Customer";
+    }
+}
